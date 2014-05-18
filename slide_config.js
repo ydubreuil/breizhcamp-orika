2436f1a9bf8f767e9d279eb1d4aa3ce16569dff9
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2014-05-18 14:21:22 +0200

    style for BreizhCamp presentation

--- slide_config.js
+++ slide_config.js
@@ -1,30 +1,26 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventTitle: 'Google I/O 2013',
+    title: 'Orika',
+    subtitle: 'mapping d\'objets Java sans réflexion',
+    eventTitle: 'BreizhCamp 2014',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/root-shell.ico',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
     ],
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Yoann Dubreuil',
+    company: 'Développeur, Orange IT&Labs',
+    gplus: 'http://plus.google.com/109669109238504787835',
+    github: 'http://github.com/ydubreuil'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
@@ -34,4 +30,3 @@ var SLIDE_CONFIG = {
     github: 'http://github.com/you'
   }*/]
 };
-
